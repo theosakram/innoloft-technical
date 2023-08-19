1f0f446a42aba6c312961ddc91170b98636dde96
Sakra Personal <wyrdhn@gmail.com>
2023-08-19 22:08:09 +0700

    adding draft-js and react-draft-wyswyg

--- src/app/product/edit/layout.tsx
+++
@@ -1,5 +0,0 @@
-const EditProductLayout = () => {
-  return <h1>Hehe</h1>;
-};
-
-export default EditProductLayout;
--- src/app/product/edit/page.tsx
+++ src/app/product/edit/page.tsx
@@ -1,5 +1,21 @@
+'use client';
+
+import { useState } from 'react';
+import { Editor } from 'react-draft-wysiwyg';
+import { EditorState } from 'draft-js';
+import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
+
 const ProductEditPage = () => {
-  return <h1>Product Edit</h1>;
+  const [editorState, setEditorState] = useState(() =>
+    EditorState.createEmpty(),
+  );
+
+  return (
+    <div>
+      <h1>Product Edit</h1>
+      <Editor editorState={editorState} onEditorStateChange={setEditorState} />
+    </div>
+  );
 };
 
 export default ProductEditPage;
